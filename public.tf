7dec96d4b8e6c62929f59e172d0e7bc480dbaccf
Henrique <hvenancio@outlook.com>
2025-06-21 09:51:21 -0300

    feature: adding subnet and variables

---
+++ public.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet("${aws_vpc.eks_vpc.cidr_block}", 8, 1)
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devopsdemo-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet("${aws_vpc.eks_vpc.cidr_block}", 8, 2)
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devopsdemo-subnet-1b"
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "env_name" {
   type = string
 }
+
+variable "cidr_block_vpc" {
+  type        = string
+  description = "Networking CIDR block to be used for VPC"
+}
