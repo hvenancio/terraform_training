82379ae6c853b1ce68c9e641dc6d64b7082839df
Henrique <hvenancio@outlook.com>
2025-06-21 09:19:24 -0300

    fix:v´c.tf

---
+++ s3-config-bucket.tf
@@ -0,0 +1,10 @@
+# Null
+# resource "aws_s3_bucket" "s3_config_bucket" {
+#   bucket = "devopsdemo-${var.env_name}"
+#   region = "us-east-1"
+
+#   tags = {
+#     Name        = "devopsdemo-${var.env_name}"
+#     Environment = "${var.env_name}"
+#   }
+# }
