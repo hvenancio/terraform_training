f80a561ba9f31f73d2445ce2de4eaa6218ca0dd4
Henrique <hvenancio@outlook.com>
2025-06-22 16:26:03 -0300

    feat: Creating TLS certificate, OIDC certificate and connect provider

--- output.tf
+++ output.tf
@@ -1,19 +1,3 @@
-output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a
-}
-
-output "subnet_pub_1b" {
-  value = module.eks_network.subnet_pub_1b
-}
-
-output "subnet_private_1a" {
-  value = module.eks_network.subnet_private_1a
-}
-
-output "subnet_private_1b" {
-  value = module.eks_network.subnet_private_1b
-}
-
-output "eks_vpc_config" {
-  value = module.eks_cluster.eks_vpc_config
+output "oidc" {
+  value = module.eks_cluster.oidc
 }
