a114b5d9ab495e0986469b3bee04d9f8ae256c76
Henrique <hvenancio@outlook.com>
2025-06-22 13:28:16 -0300

    feat: Creating the EKS cluster

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a.id
+  value = module.eks_network.subnet_pub_1a
 }
 
 output "subnet_pub_1b" {
-  value = module.eks_network.subnet_pub_1b.id
+  value = module.eks_network.subnet_pub_1b
 }
 
 output "subnet_private_1a" {
-  value = module.eks_network.subnet_private_1a.id
+  value = module.eks_network.subnet_private_1a
 }
 
 output "subnet_private_1b" {
-  value = module.eks_network.subnet_private_1b.id
+  value = module.eks_network.subnet_private_1b
 }
